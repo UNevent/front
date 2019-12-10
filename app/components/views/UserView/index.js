865bf086fc78880596c3bac090755c4422025af2
Dfrodriguez <dafrodriguezro@unal.edu.co>
2019-12-10 03:54:58 -0500

    fix

--- app/components/views/UserView/index.js
+++ app/components/views/UserView/index.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { View, Text, ImageBackground, StatusBar, Image, StyleSheet, ScrollView, KeyboardAvoidingView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, ImageBackground } from "react-native";
 import styles  from './styles';
 import Images from '../../../assets/Images';
-import { Header } from 'react-native-elements';
-import ButtonIcon from "../../common/ButtonIcon";
 import AvatarIcon from "../../common/AvatarIcon";
+import LoadingView from '../LoadingView';
+
+import { useSelector } from 'react-redux';
+import { validateSession } from "../../common/Authentication";
+import { getSession } from '../../../store/selectors'
+
+async const getAuthData = (token, client, user) => {
+	return await validateSession(token, client, user);
+}
 
 const UserView = ({goToHome}) => {
-  	return (	    
+	[loaded, setLoaded] = useState(false);
+	const auth_selector = useSelector(getSession);
+
+	getAuthData(auth_selector.token, auth_selector.client, auth_selector.user).then(
+		(response) => console.log
+	);
+
+	if(loaded){
+		return (	    
 			<ImageBackground source={Images.backmap} style={styles.backmap}>
 					<View style={styles.user}>
 							<AvatarIcon style={ styles.avatar}
-								title={"db"}
 							/>
 							<View style={styles.userInfo} >
 								<Text style={styles.userName}>Daniel Felipe Bueno</Text>
@@ -19,11 +33,14 @@ const UserView = ({goToHome}) => {
 							</View>
 					</View>
 					<View style={styles.userPreferences}>
-
+	
 					</View>
 				
-	    </ImageBackground>
-  	);
+			</ImageBackground>
+		);
+	}
+	
+	return <LoadingView/>
 };
 
 export default UserView;
